23bd14826afb2d27249b11c7d07ed28b54d98610
aravindvnair99 <keerthanachowdary218@gmail.com>
2020-04-13 18:10:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/login1/Login_Form.java
@@ -0,0 +1,15 @@
+package com.example.login1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_Form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login__form);
+
+    }
+}
---
+++ app/src/main/java/com/example/login1/Signup_Form.java
@@ -0,0 +1,16 @@
+package com.example.login1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Signup_Form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup__form);
+        getSupportActionBar().setTitle("Login Form");
+    }
+}
